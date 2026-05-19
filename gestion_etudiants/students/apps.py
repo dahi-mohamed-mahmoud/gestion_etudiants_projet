07fa35d8ecff9e91de1e8872d52c0e6e4fd79952
BROSTORNADO <b.cheikhahmed98@gmail.com>
2026-05-19 23:58:21 +0000

    Add database migration and seeding logic for student filieres

--- gestion_etudiants/students/apps.py
+++ gestion_etudiants/students/apps.py
@@ -5,3 +5,31 @@ class StudentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'students'
     verbose_name = 'Gestion des étudiants'
+
+    def ready(self):
+        from django.db.models.signals import post_migrate
+
+        def seed_filieres(sender, **kwargs):
+            if sender.name != 'students':
+                return
+
+            from .models import Filiere
+
+            defaults = [
+                ("INFO", "Informatique", "Filière Informatique"),
+                ("GEST", "Gestion", "Filière Gestion"),
+                ("ELEC", "Électronique", "Filière Électronique"),
+            ]
+
+            for code, nom, description in defaults:
+                Filiere.objects.get_or_create(
+                    code=code,
+                    defaults={"nom": nom, "description": description},
+                )
+
+        post_migrate.connect(
+            seed_filieres,
+            sender=self,
+            dispatch_uid='students.seed_filieres',
+            weak=False,
+        )
