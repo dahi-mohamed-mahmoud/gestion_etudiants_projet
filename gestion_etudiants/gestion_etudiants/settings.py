7a0a1da9696561eb6f2f4284bd289daa5afea035
BROSTORNADO <b.cheikhahmed98@gmail.com>
2026-05-19 22:37:27 +0000

    Refactor environment configuration and database settings for improved flexibility

--- gestion_etudiants/gestion_etudiants/settings.py
+++ gestion_etudiants/gestion_etudiants/settings.py
@@ -49,13 +49,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gestion_etudiants.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    
-}
-}
+if os.environ.get('POSTGRES_HOST'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('POSTGRES_DB', 'gestion_etudiants'),
+            'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+            'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+            'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
